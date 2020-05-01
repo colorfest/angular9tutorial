3dc5fa22c793f7804e2dbd90378a14d36d73bae7
K.C. Hunter <djvillain@hotmail.com>
2020-05-01 14:21:16 -0400

    Lesson 4: String Interpolation and Properties

--- src/app/components/president-demo/president-demo.component.ts
+++ src/app/components/president-demo/president-demo.component.ts
@@ -7,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PresidentDemoComponent implements OnInit {
 
-  constructor() { }
+  // create president properties
+  firstName: string;
+  lastName: string;
+  age: number;
+  presID: number;
+  hasChildren: boolean;
+  wives: string[];
+  numberOfTweets: number[];
+  mixedArray: any[];
+  tupleArray: [boolean, number, string];
+
+  constructor() {
+    this.firstName = `Donald`;
+    this.lastName = `Trump`;
+    this.age = 73;
+    this.presID = 45;
+
+    this.hasChildren = true;
+    this.wives = [`Melania Krauss`, `Marla Maples`, `Ivana Zelnickova`];
+    this.numberOfTweets = [12, 5, 23, 5];
+    this.mixedArray = [true, true, `Bernie`, `Biden`, `USA`, 0, false, `New York`];
+    this.tupleArray = [false, 100, `Pence`];
+  }
 
   ngOnInit(): void {
   }
