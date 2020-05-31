a985ccc04b86ec8165d07d8121357cfccae7e0b6
K.C. Hunter <djvillain@hotmail.com>
2020-05-31 13:01:36 -0400

    Angular Routing Lesson Intro.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PresidentsComponent } from './components/presidents/presidents.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'presidents', component: PresidentsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PresidentDemoComponent } from './components/president-demo/president-demo.component';
 import { PresidentsComponent } from './components/presidents/presidents.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PresidentDemoComponent,
-    PresidentsComponent
+    PresidentsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
